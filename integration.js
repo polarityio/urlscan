79998c4169abe227dea6b068c29fe1a71271f167
Joseph Miller <joem@breachintelligence.com>
2020-07-20 17:44:30 -0400

    Update terminology, dependencies and defaults.

--- integration.js
+++ integration.js
@@ -11,8 +11,8 @@ let Logger;
 let requestWithDefaults;
 let previousDomainRegexAsString = '';
 let previousIpRegexAsString = '';
-let domainBlacklistRegex = null;
-let ipBlacklistRegex = null;
+let domainBlocklistRegex = null;
+let ipBlocklistRegex = null;
 
 const MAX_DOMAIN_LABEL_LENGTH = 63;
 const MAX_DOMAIN_LENGTH = 253;
@@ -46,28 +46,28 @@ function startup(logger) {
   requestWithDefaults = request.defaults(defaults);
 }
 
-function _setupRegexBlacklists(options) {
-  if (options.domainBlacklistRegex !== previousDomainRegexAsString && options.domainBlacklistRegex.length === 0) {
-    Logger.debug('Removing Domain Blacklist Regex Filtering');
+function _setupRegexBlocklists(options) {
+  if (options.domainBlocklistRegex !== previousDomainRegexAsString && options.domainBlocklistRegex.length === 0) {
+    Logger.debug('Removing Domain Blocklist Regex Filtering');
     previousDomainRegexAsString = '';
-    domainBlacklistRegex = null;
+    domainBlocklistRegex = null;
   } else {
-    if (options.domainBlacklistRegex !== previousDomainRegexAsString) {
-      previousDomainRegexAsString = options.domainBlacklistRegex;
-      Logger.debug({ domainBlacklistRegex: previousDomainRegexAsString }, 'Modifying Domain Blacklist Regex');
-      domainBlacklistRegex = new RegExp(options.domainBlacklistRegex, 'i');
+    if (options.domainBlocklistRegex !== previousDomainRegexAsString) {
+      previousDomainRegexAsString = options.domainBlocklistRegex;
+      Logger.debug({ domainBlocklistRegex: previousDomainRegexAsString }, 'Modifying Domain Blocklist Regex');
+      domainBlocklistRegex = new RegExp(options.domainBlocklistRegex, 'i');
     }
   }
 
-  if (options.ipBlacklistRegex !== previousIpRegexAsString && options.ipBlacklistRegex.length === 0) {
-    Logger.debug('Removing IP Blacklist Regex Filtering');
+  if (options.ipBlocklistRegex !== previousIpRegexAsString && options.ipBlocklistRegex.length === 0) {
+    Logger.debug('Removing IP Blocklist Regex Filtering');
     previousIpRegexAsString = '';
-    ipBlacklistRegex = null;
+    ipBlocklistRegex = null;
   } else {
-    if (options.ipBlacklistRegex !== previousIpRegexAsString) {
-      previousIpRegexAsString = options.ipBlacklistRegex;
-      Logger.debug({ ipBlacklistRegex: previousIpRegexAsString }, 'Modifying IP Blacklist Regex');
-      ipBlacklistRegex = new RegExp(options.ipBlacklistRegex, 'i');
+    if (options.ipBlocklistRegex !== previousIpRegexAsString) {
+      previousIpRegexAsString = options.ipBlocklistRegex;
+      Logger.debug({ ipBlocklistRegex: previousIpRegexAsString }, 'Modifying IP Blocklist Regex');
+      ipBlocklistRegex = new RegExp(options.ipBlocklistRegex, 'i');
     }
   }
 }
@@ -88,12 +88,12 @@ function doLookup(entities, options, cb) {
   let lookupResults = [];
   let tasks = [];
 
-  _setupRegexBlacklists(options);
+  _setupRegexBlocklists(options);
 
   Logger.debug(entities);
 
   entities.forEach((entity) => {
-    if (!_isInvalidEntity(entity) && !_isEntityBlacklisted(entity, options)) {
+    if (!_isInvalidEntity(entity) && !_isEntityBlocklisted(entity, options)) {
       let requestOptions = {
         uri: `${options.host}/v1/search`,
         method: 'GET',
@@ -348,28 +348,28 @@ function _isInvalidEntity(entity) {
   return false;
 }
 
-function _isEntityBlacklisted(entity, options) {
-  const blacklist = options.blacklist;
+function _isEntityBlocklisted(entity, options) {
+  const blocklist = options.blocklist;
 
-  Logger.trace({ blacklist: blacklist }, 'checking to see what blacklist looks like');
+  Logger.trace({ blocklist: blocklist }, 'checking to see what blocklist looks like');
 
-  if (_.includes(blacklist, entity.value.toLowerCase())) {
+  if (_.includes(blocklist, entity.value.toLowerCase())) {
     return true;
   }
 
   if (entity.isIP && !entity.isPrivateIP) {
-    if (ipBlacklistRegex !== null) {
-      if (ipBlacklistRegex.test(entity.value)) {
-        Logger.debug({ ip: entity.value }, 'Blocked BlackListed IP Lookup');
+    if (ipBlocklistRegex !== null) {
+      if (ipBlocklistRegex.test(entity.value)) {
+        Logger.debug({ ip: entity.value }, 'Blocked BlockListed IP Lookup');
         return true;
       }
     }
   }
 
   if (entity.isDomain) {
-    if (domainBlacklistRegex !== null) {
-      if (domainBlacklistRegex.test(entity.value)) {
-        Logger.debug({ domain: entity.value }, 'Blocked BlackListed Domain Lookup');
+    if (domainBlocklistRegex !== null) {
+      if (domainBlocklistRegex.test(entity.value)) {
+        Logger.debug({ domain: entity.value }, 'Blocked BlockListed Domain Lookup');
         return true;
       }
     }
